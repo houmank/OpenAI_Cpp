f53ffe5bf05eb41057015820c1da514119313628
houmank <43026293+houmank@users.noreply.github.com>
2022-10-01 17:00:28 -0400

    setup initial cmakelist

---
+++ include/OpenAI_Cpp/open_ai.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace AI
+{
+/**
+ * @brief Client class for OpenAI_Cpp. This is what the user initializes with their API key and optional organization ID. This class will
+ * also be how most OpenAI api calls can be made.
+ */
+class OpenAI 
+{
+public:
+    void print();
+private:
+
+};
+
+}
---
+++ src/open_ai.cpp
@@ -0,0 +1,12 @@
+#include "OpenAI_Cpp/open_ai.h"
+#include <iostream>
+
+namespace AI 
+{
+
+void OpenAI::print()
+{
+	std::cout << "Testing from OpenAI\n";
+}
+
+}
