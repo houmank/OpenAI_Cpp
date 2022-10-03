b4f793a9e2528041b5647da045d0a3db0de6094b
houmank <43026293+houmank@users.noreply.github.com>
2022-10-02 20:24:29 -0400

    used ModelType scope explicitly

--- src/model_type_map.cpp
+++ src/model_type_map.cpp
@@ -1,4 +1,4 @@
-#include "model_type_map.h"
+#include "OpenAI_Cpp/model_type_map.h"
 
 namespace AI
 {
@@ -7,32 +7,32 @@ namespace AI
 		// initialize the mappings
 		this->modelToStringMap =
 		{
-			{Davinci, "davinci"},
-			{Curie, "curie"},
-			{Babbage, "babbage"},
-			{Ada, "ada"},
-			{Text_Davinci_002, "text-davinci-002"},
-			{Text_Curie_001, "text-curie-001"},
-			{Text_Babbage_001, "text-babbage-001"},
-			{Text_Ada_001, "text-ada-001"},
-			{Code_Davinci_002, "code-davinci-002"},
-			{Code_Cushman_001, "code-cushman-001"},
-			{Content_Filter_Alpha, "content-filter-alpha"}
+			{ModelType::Davinci, "davinci"},
+			{ModelType::Curie, "curie"},
+			{ModelType::Babbage, "babbage"},
+			{ModelType::Ada, "ada"},
+			{ModelType::Text_Davinci_002, "text-davinci-002"},
+			{ModelType::Text_Curie_001, "text-curie-001"},
+			{ModelType::Text_Babbage_001, "text-babbage-001"},
+			{ModelType::Text_Ada_001, "text-ada-001"},
+			{ModelType::Code_Davinci_002, "code-davinci-002"},
+			{ModelType::Code_Cushman_001, "code-cushman-001"},
+			{ModelType::Content_Filter_Alpha, "content-filter-alpha"}
 		};
 
 		this->stringToModelMap =
 		{
-			{"davinci", Davinci},
-			{"curie", Curie},
-			{"babbage", Babbage},
-			{"ada", Ada},
-			{"text-davinci-002", Text_Davinci_002},
-			{"text-curie-001", Text_Curie_001},
-			{"text-babbage-001", Text_Babbage_001},
-			{"text-ada-001", Text_Ada_001},
-			{"code-davinci-002", Code_Davinci_002},
-			{"code-cushman-001", Code_Cushman_001},
-			{"content-filter-alpha", Content_Filter_Alpha}
+			{"davinci", ModelType::Davinci},
+			{"curie", ModelType::Curie},
+			{"babbage", ModelType::Babbage},
+			{"ada", ModelType::Ada},
+			{"text-davinci-002", ModelType::Text_Davinci_002},
+			{"text-curie-001", ModelType::Text_Curie_001},
+			{"text-babbage-001", ModelType::Text_Babbage_001},
+			{"text-ada-001", ModelType::Text_Ada_001},
+			{"code-davinci-002", ModelType::Code_Davinci_002},
+			{"code-cushman-001", ModelType::Code_Cushman_001},
+			{"content-filter-alpha", ModelType::Content_Filter_Alpha}
 		};
 	}
 
