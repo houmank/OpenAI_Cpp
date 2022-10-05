78c7561f9430ca01f9e79fd2f6dd025b19fe0d83
houmank <43026293+houmank@users.noreply.github.com>
2022-10-04 22:08:00 -0400

    OpenAI gets internal model class that it will use when calling endpoints
    This is so we can do either builder style design or simply keep it to where OpenAI calls the endpoints for you. Still have not decided if i want to use builder design pattern.

--- include/OpenAI_Cpp/open_ai.h
+++ include/OpenAI_Cpp/open_ai.h
@@ -5,6 +5,8 @@
 #include "cpr/cpr.h"
 #include "nlohmann/json.hpp"
 
+#include "OpenAI_Cpp/Models/models.h"
+
 namespace AI
 {
 /**
@@ -26,10 +28,44 @@ public:
     */
     OpenAI(const std::string& api_key, const std::string& org_id = "");
 
+    /**
+    * Model is a builder function that returns a Model&, this model is what is currently used when calling different endpoints.
+    * This method accepts two overloads. this method accepts an id string which dictates what openai model is used. User can also use 
+    * the ModelType enum to indicate the id. If an invalid id was passed, the method throws InvalidArgumentException.
+    * 
+    * @param id the id of the model which will be used.
+    * @return const referrence to the current model. referrence used so builder pattern can be used.
+    * @throws InvalidArgumentException
+    */
+    const AI::Model& Model(const std::string& id);
+
+    /**
+    * Model is a builder function that returns a Model&, this model is what is currently used when calling different endpoints.
+    * This method accepts two overloads. this method accepts a ModelType enum to indicate the id. There is also an overload for
+    * passing the id in directly using a string. If an invalid modeltype was passed, the method throws InvalidArgumentException.
+    * 
+    * @param id the id of the model which will be used.
+    * @return const referrence to the current model. referrence used so builder pattern can be used.
+    * @throws InvalidArgumentException
+    */
+    const AI::Model& Model(AI::ModelType model);
+
+private:
+	/**
+	* @brief initModel is a private initializer function that will fill out the necessary information for a given model using
+	* openai's GET model/{model}. If an error occures such as not being able to find a named model using its id, the function will return false
+	* else it will return true.
+	* 
+	* @return boolean indicates if the initialization of this object was successful
+	*/
+	void initModel();
+
 private:
     
     const std::string api_key_auth;
     const std::string org_identifier;
+
+    std::unique_ptr<AI::Model> currentModel;
 };
 
 }
--- src/open_ai.cpp
+++ src/open_ai.cpp
@@ -1,12 +1,38 @@
 #include "OpenAI_Cpp/open_ai.h"
+#include <iostream>
 
 namespace AI 
 {
-	const std::string OPENAI_API_URL = R"(https://api.openai.com/v1/)";
-	const std::string OPENAI_API_ORG_HEADER = "OpenAI-Organization";
+	static const std::string OPENAI_API_URL = R"(https://api.openai.com/v1/)";
+	static const std::string OPENAI_API_ORG_HEADER = "OpenAI-Organization";
 
 OpenAI::OpenAI(const std::string& api_key, const std::string& org_id)
-	: api_key_auth(api_key), org_identifier(org_id)
+	: api_key_auth(api_key), org_identifier(org_id), currentModel(nullptr)
 {}
+
+const Model& OpenAI::Model(const std::string& id)
+{
+	this->currentModel = std::make_unique<AI::Model>(id);
+	initModel();
+
+	return *this->currentModel;
+}
+
+const AI::Model& OpenAI::Model(AI::ModelType model)
+{
+	this->currentModel = std::make_unique<AI::Model>(model);
+	initModel();
+
+	return *this->currentModel;
+}
+
+void OpenAI::initModel()
+{
+	std::string url = OPENAI_API_URL + R"(models/)" + this->currentModel->id;
+	cpr::Response modelResponse = cpr::Get(cpr::Url(url),
+		                          cpr::Bearer(this->api_key_auth));
+
+	std::cout << modelResponse.text << std::endl;
+}
 
 }
