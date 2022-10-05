c6241785d155b3182170f448c7ff47d88c0263ff
houmank <43026293+houmank@users.noreply.github.com>
2022-10-04 22:22:02 -0400

    updated openai test to now load the secret key so tests can actually call api with key

--- test/openai_test.cpp
+++ test/openai_test.cpp
@@ -1,6 +1,39 @@
 #include "gtest/gtest.h"
+#include "OpenAI_Cpp/open_ai.h"
 
-TEST(initialTests, TestingGtestLink)
+#include <fstream>
+#include <memory>
+
+static const std::string API_KEY_FP = R"(test\OpenAI_API_SK.txt)";
+static std::string secret;
+
+class OpenAI_Fixture : public ::testing::Test
 {
-	ASSERT_TRUE(false);
+public:
+
+	static void SetUpTestSuite()
+	{
+		// Load secret key for OpenAI object creation
+		std::ifstream ifstream;
+		ifstream.open(API_KEY_FP);
+		if (ifstream.is_open())
+		{
+			std::getline(ifstream, secret);
+			ifstream.close();
+		}
+		
+	}
+
+	virtual void SetUp() override
+	{
+		openai = std::make_unique<AI::OpenAI>(secret);
+	}
+
+	
+	std::unique_ptr<AI::OpenAI> openai = nullptr;
+};
+
+TEST_F(OpenAI_Fixture, ModelBuilder)
+{
+	openai->Model(AI::ModelType::Text_Davinci_002);
 }
