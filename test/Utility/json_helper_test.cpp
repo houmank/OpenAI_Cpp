1573df4c2738bb807fb11838b4eb3b6b45bbf93a
houmank <43026293+houmank@users.noreply.github.com>
2022-10-11 17:44:58 -0400

    added tests to ensure proper parsing of model and permission objects
    These two functions do minimal error checking and caller of these functions must still remember to call the schema functions to ensure json is in the correct format. If improper json format is passed in the assertions will trigger or in release build undefined behavior.

--- test/Utility/json_helper_test.cpp
+++ test/Utility/json_helper_test.cpp
@@ -42,26 +42,72 @@ TEST(errorMessageInJsonResponse, GoodErrorJsonPassedIn)
 	ASSERT_TRUE(message.find("need to provide your API key") != std::string::npos);
 }
 
-TEST(deserializePermissionFromJson, BadPermissionParsed)
-{
-	// TODO tomorrow
-	ASSERT_FALSE(true);
-}
-
 TEST(deserializePermissionFromJson, GoodPermissionParsed)
 {
-	// TODO tomorrow
-	ASSERT_FALSE(true);
-}
+	// User should check for bad input with JSON schemas in json_schemas.h
+	const char* raw_json =
+		R"({
+          "permission": [
+            {
+              "id": "modelperm-id123",
+              "object": "model_permission",
+              "created": 1664307523,
+              "allow_create_engine": false,
+              "allow_sampling": true,
+              "allow_logprobs": true,
+              "allow_search_indices": false,
+              "allow_view": true,
+              "allow_fine_tuning": false,
+              "organization": "*",
+              "group": null,
+              "is_blocking": false
+            }
+          ]
+        })";
 
-TEST(deserializeModelFromJson, BadModelParsed)
-{
-	// TODO tomorrow
-	ASSERT_FALSE(true);
+	rapidjson::Document json;
+	json.Parse(raw_json);
+	rapidjson::Value* permission = rapidjson::GetValueByPointer(json, "/permission");
+
+	std::variant<std::unique_ptr<AI::Permission>, AI::Error> result = AI::deserializePermissionFromJson(permission);
+	std::unique_ptr<AI::Permission>* myPerm = std::get_if <std::unique_ptr<AI::Permission>>(&result);
+	ASSERT_TRUE(myPerm != nullptr);
+	ASSERT_TRUE((*myPerm)->created == 1664307523);
 }
 
 TEST(deserializeModelFromJson, GoodModelParsed)
 {
-	// TODO tomorrow
-	ASSERT_FALSE(true);
+	// User should check for bad input with JSON schemas in json_schemas.h
+	const char* raw_json = R"({
+          "id": "text-davinci-002",
+          "object": "model",
+          "created": 1649880484,
+          "owned_by": "openai",
+          "permission": [
+            {
+              "id": "modelperm-7E53j9OtnMZggjqlwMxW4QG7",
+              "object": "model_permission",
+              "created": 1664307523,
+              "allow_create_engine": false,
+              "allow_sampling": true,
+              "allow_logprobs": true,
+              "allow_search_indices": false,
+              "allow_view": true,
+              "allow_fine_tuning": false,
+              "organization": "*",
+              "group": null,
+              "is_blocking": false
+            }
+          ],
+          "root": "text-davinci-002",
+          "parent": null
+        })";
+
+	rapidjson::Document json;
+	json.Parse(raw_json);
+
+	std::unique_ptr<AI::Model> modelObj = std::make_unique<AI::Model>("text-davinci-002");
+	AI::Error possibleError = AI::deserializeModelFromJson(&json, modelObj);
+	ASSERT_TRUE(possibleError.type == AI::ErrorType::NO_ERR);
+	ASSERT_TRUE(modelObj->getCreated() == 1649880484);
 }
